64c9059907e71d0753331f67da0f6e065fab24a9
Anthelion <129961957+anthelion62@users.noreply.github.com>
2023-09-10 22:28:19 +0900

    Update step1(switch).java

--- step1(switch).java
+++ step1(switch).java
@@ -1,1 +1,23 @@
 
+public class step1 {
+    public static void main(String[] args) {
+        //画面表示；
+        String numberSelect;
+        System.out.println("株式取引管理システムを開始します");
+        do {
+            System.out.println("操作するメニューを選んでください");
+            System.out.println("1.銘柄マスタ一覧表示");
+            System.out.println("2.銘柄マスタ新規登録");
+            System.out.println("9.アプリケーションを終了する");
+            System.out.println("番号を入力してください：");
+            //ユーザー入力；
+            numberSelect = new java.util.Scanner(System.in).nextLine();
+            switch (numberSelect) {
+                case "1" -> System.out.println("「銘柄マスタ一覧表示」が選択されました。");
+                case "2" -> System.out.println("「銘柄マスタ新規登録」が選択されました。");
+                case "9" -> System.out.println("アプリケーションを終了します。");
+                default -> System.out.println("“" + numberSelect + "”に対応するメニューは存在しません。");
+            }
+        }while (!numberSelect.equals("9"));
+    }
+}
